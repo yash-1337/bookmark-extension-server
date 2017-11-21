018cf11d9da7c9af25ee98e7af428453d0064128
Yash Patel <yash.patel0419@gmail.com>
2017-11-21 16:05:14 -0600

    Updated the server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-  res.send('hello!');
+  res.send('This server is to be used with the bookmark extension for Google Chrome.');
 });
 
 app.get('/getBookmarks', (req, res) => {
